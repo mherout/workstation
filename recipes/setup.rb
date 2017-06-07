391e8a1d9fa8701096da4f1f2a385e999467d7ae
ChefDK User <chef@ip-10-230-211-87.ec2.internal>
2017-06-07 09:13:54 +0000

    07/06/2017 - Añadimos a receta setup.rb

---
+++ recipes/setup.rb
@@ -0,0 +1,15 @@
+######################################
+### - RECETAS Martes 06 Xuñ 2017 - ###
+#
+#
+package 'tree' do
+ action :install
+end
+
+file "/etc/motd"  do
+content "Propiedade de manolez corporation"
+end
+#
+#
+######################################
+
