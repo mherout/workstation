04dbe380e30b5ced7400f44c0e8c7bcfac375ef4
ChefDK User <chef@ip-10-230-211-87.ec2.internal>
2017-06-07 09:46:44 +0000

    07/06 - Cambio de usuario y grupo para fichero motd

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -2,13 +2,18 @@
 ### - RECETAS Martes 06 Xuñ 2017 - ###
 #
 #
-package 'tree' do
- action :install
-end
+package "tree"
 
+# Cambios de usuarios y grupos
+#
 file "/etc/motd"  do
 content "Propiedade de manolez corporation"
+
+ owner "root"
+ group "root"
 end
+
+
 #
 #
 ######################################
